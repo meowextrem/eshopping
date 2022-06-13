23c654a932f5692e094e15312ef236438e2a0623
Julius Robert Javier <juliusrobertjavier@gmail.com>
2022-06-13 17:00:34 +0800

    Bug fixing , remove the while counter

--- application/views/pages/products.php
+++ application/views/pages/products.php
@@ -34,28 +34,26 @@
 
                 <?php } else { ?>
 
-                    <?php $count = 0; ?>
-                    <?php while ($count < 3) { ?>
-                        <?php foreach($all_featured_products as $featured_product) { ?>
-                            <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
-                                <div class="card product-item border-0 mb-4">
-                                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                        <img class="img-fluid w-120" src="<?php echo HOST_API_IMAGE_FOLDER_URL.$featured_product['image']?>" alt="">
-                                    </div>
-                                    <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                                        <h6 class="text-truncate mb-3"><?php echo $featured_product['thename']; ?></h6>
-                                        <div class="d-flex justify-content-center">
-                                            <h6>$<?php echo $featured_product['price']; ?></h6><h6 class="text-muted ml-2"><del>$<?php echo $featured_product['price']; ?></del></h6>
-                                        </div>
-                                    </div>
-                                    <div class="card-footer d-flex justify-content-between bg-light border">
-                                        <a class="btn btn-sm text-dark p-0"></a>
-                                        <a href="<?php echo site_url().'add_to_cart/'.$featured_product['id']?>" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                    
+                    <?php foreach($all_featured_products as $featured_product) { ?>
+                        <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                            <div class="card product-item border-0 mb-4">
+                                <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                    <img class="img-fluid w-120" src="<?php echo HOST_API_IMAGE_FOLDER_URL.$featured_product['image']?>" alt="">
+                                </div>
+                                <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                                    <h6 class="text-truncate mb-3"><?php echo $featured_product['thename']; ?></h6>
+                                    <div class="d-flex justify-content-center">
+                                        <h6>$<?php echo $featured_product['price']; ?></h6><h6 class="text-muted ml-2"><del>$<?php echo $featured_product['price']; ?></del></h6>
                                     </div>
                                 </div>
+                                <div class="card-footer d-flex justify-content-between bg-light border">
+                                    <a class="btn btn-sm text-dark p-0"></a>
+                                    <a href="<?php echo site_url().'add_to_cart/'.$featured_product['id']?>" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                                </div>
                             </div>
-                        <?php $count++; ?>
-                        <?php } ?>
+                        </div>
+                        
                     <?php } ?>
                     
 
